82fc38de81fb55e821af4f183ace189c52b7b02f
Denis Kornienko <korney197823@gmail.com>
2016-08-11 14:39:29 +0400

    Finished jquery animation

--- js/common.js
+++ js/common.js
@@ -1,1 +1,26 @@
-common.js
+$(document).ready(function() {
+	$('.accordeon__trigger').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+        console.log('Hi');
+		var $this = $(this),
+		    item = $this.closest('.accordeon__item'),
+		    list = $this.closest('.accordeon__list'),
+		    items = list.find('.accordeon__item'),
+		    content = item.find('.accordeon__inner'),
+		    otherContent = list.find('.accordeon__inner'),
+		    duration = 300;
+
+		    if (!item.hasClass('active')) {
+		    	items.removeClass('active')
+		    	item.addClass('active');
+
+		    	otherContent.stop(true, true).slideUp(duration);
+		    	content.stop(true, true).slideDown(duration);
+		    }
+		    else {
+		    	content.stop(true, true).slideUp(duration);
+		    	item.stop(true, true).removeClass('active');
+		    }
+	});
+});
